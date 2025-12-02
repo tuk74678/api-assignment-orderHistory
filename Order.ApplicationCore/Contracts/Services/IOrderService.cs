cc50ddcb3966619e921de2d7df9ac229acee13be
Leapheng Keam <leaphengkeam@gmail.com>
2025-12-02 00:01:29 -0500

    created Order Service class with its interface

---
+++ Order.ApplicationCore/Contracts/Services/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using Order.ApplicationCore.Entities;
+
+namespace Order.ApplicationCore.Contracts.Services;
+
+public interface IOrderService
+{
+    Task<Orders> InsertOrderAsync(Orders order);
+    Task<Orders> UpdateOrderAsync(Orders order);
+    Task<Orders> DeleteOrderAsync(int id);
+    Task<IEnumerable<Orders>> GetAllOrdersAsync();
+    Task<Orders> GetOrderByIdAsync(int id);
+    Task<IEnumerable<Orders>> GetOrderByCustomerIdAsync(int customerId);
+}
---
+++ Order.Infrastructure/Services/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using Order.ApplicationCore.Contracts.Repositories;
+using Order.ApplicationCore.Contracts.Services;
+using Order.ApplicationCore.Entities;
+
+namespace Order.Infrastructure.Services;
+
+public class OrderService: IOrderService
+{
+    private readonly IOrderRepository _orderRepository;
+    public OrderService(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+    }
+    
+    public Task<Orders> InsertOrderAsync(Orders order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Orders> UpdateOrderAsync(Orders order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Orders> DeleteOrderAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Orders>> GetAllOrdersAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Orders> GetOrderByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Orders>> GetOrderByCustomerIdAsync(int customerId)
+    {
+        throw new NotImplementedException();
+    }
+}
