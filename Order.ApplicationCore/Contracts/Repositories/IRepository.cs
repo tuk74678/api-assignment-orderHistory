ab7539d4194a04ec05054cf5fc7a9dfa5480c73f
Leapheng Keam <leaphengkeam@gmail.com>
2025-12-01 23:08:47 -0500

    created a base repository and its interface for all CRUD operations

---
+++ Order.ApplicationCore/Contracts/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Order.ApplicationCore.Contracts.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> Insert(T entity);
+    Task<T> Update(T entity);
+    Task<T> Delete(int id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+}
---
+++ Order.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Order.ApplicationCore.Contracts.Repositories;
+using Order.Infrastructure.Data;
+
+namespace Order.Infrastructure.Repositories;
+
+public class BaseRepository<T>: IRepository<T> where T : class
+{
+    private readonly OrderHistoryDbContext _orderHistoryDbContext;
+    public BaseRepository(OrderHistoryDbContext orderHistoryDbContext)
+    {
+        _orderHistoryDbContext = orderHistoryDbContext;
+    }
+    public async Task<T> Insert(T entity)
+    {
+        await _orderHistoryDbContext.Set<T>().AddAsync(entity);
+        await _orderHistoryDbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> Update(T entity)
+    {
+        _orderHistoryDbContext.Entry(entity).State = EntityState.Modified;
+        await _orderHistoryDbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> Delete(int id)
+    {
+        var entity = _orderHistoryDbContext.Set<T>().Find(id);
+        if (entity != null)
+        {
+            _orderHistoryDbContext.Set<T>().Remove(entity);
+            await _orderHistoryDbContext.SaveChangesAsync();
+            return entity;
+        }
+        return null;
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _orderHistoryDbContext.Set<T>().ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _orderHistoryDbContext.Set<T>().FindAsync(id);
+    }
+}
